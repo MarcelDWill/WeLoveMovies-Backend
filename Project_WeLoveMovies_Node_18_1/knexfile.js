995185cb81a36807ff46898f3b807f0c74468b85
Marcel Williams <truu2u@yahoo.com>
2024-12-31 22:40:45 -0500

    updates

--- Project_WeLoveMovies_Node_18_1/knexfile.js
+++ Project_WeLoveMovies_Node_18_1/knexfile.js
@@ -1,15 +1,17 @@
 const path = require("path");
-
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://we_love_movies_af68_user:scjwiLvcTqWVQN5tYt6mEIZjm1bYXXef@dpg-ctq8vctds78s73d9f9o0-a.virginia-postgres.render.com/we_love_movies_af68",
+  DATABASE_URL = "postgresql://we_love_movies_af68_user:scjwiLvcTqWVQN5tYt6mEIZjm1bYXXef@dpg-ctq8vctds78s73d9f9o0-a.virginia-postgres.render.com:5432/we_love_movies_af68",
 } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -17,24 +19,21 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    ssl: {
-      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
-    }
   },
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    connection: {
+      connectionString: DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    pool: { min: 2, max: 10 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    ssl: {
-      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
-    }
   },
 
   test: {
