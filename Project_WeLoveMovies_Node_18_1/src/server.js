0c518cdc81bff872c5c321debbb68f1c6bd0b931
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2025-01-01 14:20:59 -0500

    Update server.js

--- Project_WeLoveMovies_Node_18_1/src/server.js
+++ Project_WeLoveMovies_Node_18_1/src/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+
 const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
-app.use(cors());
+
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
